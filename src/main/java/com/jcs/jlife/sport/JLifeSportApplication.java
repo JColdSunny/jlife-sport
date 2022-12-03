3441f8d65d278e7763a7eb8211914eb7d43acc6a
Dmitri Bahaslouski <dbahaslouski@deem.com>
2022-12-03 12:03:37 +0300

    init project

---
+++ src/main/java/com/jcs/jlife/sport/JLifeSportApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.jlife.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLifeSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JLifeSportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcs/jlife/sport/JLifeSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.jlife.sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JLifeSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
